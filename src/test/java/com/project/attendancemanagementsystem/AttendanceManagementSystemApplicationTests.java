6f0388060dee0fc1ff61ca0460b680ba0aec0fb9
tetianadivnych <divnatany17@gmail.com>
2022-12-08 16:39:02 +0200

    init project

---
+++ src/test/java/com/project/attendancemanagementsystem/AttendanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.attendancemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
